92aaac6884601c692497cb811725361bf6c266b1
patriceckhart <mail@patriceckhart.com>
2026-01-07 16:54:44 +0100

    dist removed

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { ResourceResponse, AddOptions } from './types';
-export declare function add(file: Buffer | string, fileName: string, options?: AddOptions): Promise<ResourceResponse>;
-export declare function find(identifier: string): Promise<Buffer | null>;
-export { ResourceResponse, AddOptions } from './types';
-//# sourceMappingURL=index.d.ts.map
--- dist/providers/LocalStorageProvider.d.ts
+++
@@ -1,8 +0,0 @@
-import { StorageProvider, ResourceResponse, AddOptions } from '../types';
-export declare class LocalStorageProvider implements StorageProvider {
-    private basePath;
-    constructor(basePath: string);
-    add(buffer: Buffer, fileName: string, options?: AddOptions): Promise<ResourceResponse>;
-    find(identifier: string): Promise<Buffer | null>;
-}
-//# sourceMappingURL=LocalStorageProvider.d.ts.map
